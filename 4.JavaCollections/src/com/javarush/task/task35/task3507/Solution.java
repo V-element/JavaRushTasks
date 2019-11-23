46a50ae9fae2b68d0aa75b51952af52ff0d8a31a
Egor <gnevanov.egor@gmail.com>
2019-11-23 23:27:54 +0300

    Third commit

--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -8,12 +8,12 @@ ClassLoader - что это такое?
 public class Solution {
     public static void main(String[] args) {
         String s = "";
-        int f = 1;
+        int f = 1, d = 0;
         Set<? extends Animal> allAnimals = getAllAnimals(Solution.class.getProtectionDomain().getCodeSource().getLocation().getPath() + Solution.class.getPackage().getName().replaceAll("[.]", "/") + "/data");
         System.out.println(allAnimals);
     }
 
-    public static Set<? extends Animal> getAllAnimals(String pathToAnimals) {
+    private static Set<? extends Animal> getAllAnimals(String pathToAnimals) {
         return null;
     }
 }

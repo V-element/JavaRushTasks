5fe8c0dfd31fea139f311e3f6794c6a9911499ce
Egor <51029628+V-element@users.noreply.github.com>
2019-11-23 23:28:45 +0300

    Update Solution.java

--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -7,13 +7,11 @@ ClassLoader - что это такое?
 */
 public class Solution {
     public static void main(String[] args) {
-        String s = "";
-        int f = 1, d = 0;
         Set<? extends Animal> allAnimals = getAllAnimals(Solution.class.getProtectionDomain().getCodeSource().getLocation().getPath() + Solution.class.getPackage().getName().replaceAll("[.]", "/") + "/data");
         System.out.println(allAnimals);
     }
 
-    private static Set<? extends Animal> getAllAnimals(String pathToAnimals) {
+    public static Set<? extends Animal> getAllAnimals(String pathToAnimals) {
         return null;
     }
 }

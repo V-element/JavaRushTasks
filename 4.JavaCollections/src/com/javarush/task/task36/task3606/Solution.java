5021bc69e72756836a8be50da21e87b1d5075336
Egor <gnevanov.egor@gmail.com>
2019-12-01 00:25:10 +0300

    From home

--- 4.JavaCollections/src/com/javarush/task/task36/task3606/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3606/Solution.java
@@ -1,8 +1,11 @@
 package com.javarush.task.task36.task3606;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,10 +29,49 @@ public class Solution {
     }
 
     public void scanFileSystem() throws ClassNotFoundException {
+        String sep = System.getProperty("file.separator");
+        String pathName = packageName;
+        if(!(packageName.endsWith(sep))){
+            pathName = pathName.concat(sep);
+        }
+        File path = new File(pathName);
+        MyClassLoader classLoader = new MyClassLoader();
+        for (File file: path.listFiles()) {
+                if (file.getName().endsWith(".class")){
+                    hiddenClasses.add(classLoader.load(file.toPath()));
+                }
+        }
     }
 
     public HiddenClass getHiddenClassObjectByKey(String key) {
+        for (Class clazz:hiddenClasses){
+            if(clazz.getSimpleName().toLowerCase().startsWith(key.toLowerCase())){
+                Constructor[] constructors = clazz.getDeclaredConstructors();
+                for(Constructor con : constructors){
+                    if(con.getParameterTypes().length == 0){
+                        con.setAccessible(true);
+                        try {
+                            return (HiddenClass)con.newInstance();
+                        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
         return null;
+    }
+
+    public class MyClassLoader extends ClassLoader {
+        public Class<?> load(Path path) {
+            try {
+                byte[] b = Files.readAllBytes(path);
+                return defineClass(null, b, 0, b.length);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
 
--- 4.JavaCollections/src/com/javarush/task/task36/task3610/MyMultiMap.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3610/MyMultiMap.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task36.task3610;
 
 import java.io.Serializable;
+import java.sql.ClientInfoStatus;
 import java.util.*;
 
 public class MyMultiMap<K, V> extends HashMap<K, V> implements Cloneable, Serializable {
@@ -16,36 +17,87 @@ public class MyMultiMap<K, V> extends HashMap<K, V> implements Cloneable, Serial
     @Override
     public int size() {
         //напишите тут ваш код
+        int size = 0;
+        for (List<V> list : map.values()) {
+            size += list.size();
+        }
+        return size;
     }
 
     @Override
     public V put(K key, V value) {
-        //напишите тут ваш код
+        if (!map.containsKey(key)) {
+            List<V> newList = new ArrayList<V>();
+            newList.add(value);
+            map.put(key, newList);
+            return null;
+        } else {
+            List<V> list = map.get(key);
+            if (repeatCount == list.size()) {
+                list.remove(0);
+            }
+            list.add(value);
+            map.put(key, list);
+            if (list.size() > 1) {
+                return list.get(list.size() - 2);
+            } else {
+                return null;
+            }
+        }
+
     }
 
     @Override
     public V remove(Object key) {
         //напишите тут ваш код
+        Object vObject = null;
+
+        if (map.containsKey(key)) {
+            List<V> list = map.get(key);
+            if (list.size() > 0) {
+                vObject = list.get(0);
+                list.remove(0);
+                if (list.size() == 0) {
+                    map.remove(key);
+                }
+            }
+            return (V) vObject;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public Set<K> keySet() {
         //напишите тут ваш код
+        return map.keySet();
     }
 
     @Override
     public Collection<V> values() {
         //напишите тут ваш код
+        List<V> tempList = new ArrayList<V>();
+        for (List<V> list : map.values()) {
+            tempList.addAll(list);
+        }
+        return tempList;
     }
 
     @Override
     public boolean containsKey(Object key) {
         //напишите тут ваш код
+        return map.containsKey(key);
     }
 
     @Override
     public boolean containsValue(Object value) {
         //напишите тут ваш код
+        for (List<V> list : map.values()) {
+            if (list.contains(value)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override

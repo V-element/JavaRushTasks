1ca5f7dbeb09d891998c4e4e3a60795fdbb8675d
V-element <gnevanov.egor@gmail.com>
2019-12-13 09:16:09 +0300

    Test from work

--- 4.JavaCollections/src/com/javarush/task/task39/task3913/LogParser.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/LogParser.java
@@ -1,40 +1,11 @@
 package com.javarush.task.task39.task3913;
 
-import com.javarush.task.task39.task3913.query.IPQuery;
-
 import java.nio.file.Path;
-import java.util.Date;
-import java.util.Set;
 
-public class LogParser implements IPQuery {
+public class LogParser {
     private Path logDir;
 
     public LogParser(Path logDir) {
         this.logDir = logDir;
-    }
-
-    @Override
-    public int getNumberOfUniqueIPs(Date after, Date before) {
-        return 0;
-    }
-
-    @Override
-    public Set<String> getUniqueIPs(Date after, Date before) {
-        return null;
-    }
-
-    @Override
-    public Set<String> getIPsForUser(String user, Date after, Date before) {
-        return null;
-    }
-
-    @Override
-    public Set<String> getIPsForEvent(Event event, Date after, Date before) {
-        return null;
-    }
-
-    @Override
-    public Set<String> getIPsForStatus(Status status, Date after, Date before) {
-        return null;
     }
 }
